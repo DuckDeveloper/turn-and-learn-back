fb56b3385fd60f3117092cc1d8f93d31e60e2a60
DuckDeveloper <vadimsabirov20@gmail.com>
2021-11-12 17:59:45 +0500

    Initialized app

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+
+app.use(express.json({extended: true}))
+app.use(cors({
+    origin: config.get('MAIN_URL')
+}))
+
+const PORT = config.get('PORT') || 5000
+
+const start = async () => {
+    try {
+        await mongoose.connect(config.get('MONGO_URI'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    } catch (e) {
+        process.exit(1)
+    }
+}
+
+start()
